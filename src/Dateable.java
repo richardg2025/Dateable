24a042cf601825f3446ed75f727a109e7893be41
techarenz <57818506+techarenz@users.noreply.github.com>
2023-02-01 12:29:40 +0800

    Create README.md

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Dateable {
+package PACKAGE_NAME;
+
+import java.util.Scanner;
+
+public class Dateable {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your age: ");
+
+        int age = (int) scanner.nextInt();
+
+        int date = age / 2 + 7;
+
+        System.out.println("You should date someone older than " + date);
+
+    }
+
 }
+
